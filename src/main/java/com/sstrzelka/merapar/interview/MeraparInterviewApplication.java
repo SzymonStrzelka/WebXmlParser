9934e699f714b59f17386be58fa5db1465e51893
SzymonStrzelka <szymek.strzelka@gmail.com>
2019-02-02 21:26:36 +0100

    First commit

---
+++ src/main/java/com/sstrzelka/merapar/interview/MeraparInterviewApplication.java
@@ -0,0 +1,14 @@
+package com.sstrzelka.merapar.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeraparInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeraparInterviewApplication.class, args);
+	}
+
+}
+
